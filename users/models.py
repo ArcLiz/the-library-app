3826d1e6d398c28fa8ae8adc8908a5db8a3c4cfc
ArcLiz <gyllingstenlizah@gmail.com>
2023-11-14 14:10:39 +0100

    Remove Settings tab from navbar

--- users/models.py
+++ users/models.py
@@ -1,9 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-
-from django_resized import ResizedImageField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from django_resized import ResizedImageField
 
 from djrichtextfield.models import RichTextField
 
